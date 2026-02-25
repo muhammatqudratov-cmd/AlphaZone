e380cd1344526aa7bdde8ea4e5760410850395fc
Mukhammed <muhammatqudratov@gmail.com>
2026-02-25 14:59:05 +0900

    feat: mongoDB connected successfully via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import mongoose from "mongoose";
+
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("mongoDB connected successfully");
+    const PORT = process.env.PORT ?? 3000;
+    })
+.catch((er) => console.log("ERROR on your MongoDB"));
+
 
