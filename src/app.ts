f9f8cf1b2b6d43312255b9d64887d349a7e3c713
Mukhammed <muhammatqudratov@gmail.com>
2026-03-30 01:25:42 +0900

    feat: backend connected to frontend project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import cors from 'cors';  // ← QO'SH
 import path from "path";
 import router from "./router";
 import routerAdmin from './router-admin';
 import morgan from "morgan";
-import cookieParser  from "cookie-parser";
+import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from './libs/config';
-
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from './libs/types/common';
@@ -16,11 +16,12 @@ const store = new MongoDBStore({
     collection: 'sessions'
 });
 
-
-
-
 /** 1-ENTRANCE **/
 const app = express();
+app.use(cors({                    // ← QO'SH
+    origin: true,
+    credentials: true,
+}));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 app.use("/uploads", express.static("/uploads"));
@@ -28,42 +29,31 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
-
 /** 2-SESSION **/
 app.use(
     session({
         secret: String(process.env.SESSION_SECRET),
         cookie:{
-            maxAge: 1000 * 3600 * 6, // 6 hours 
+            maxAge: 1000 * 3600 * 6,
         },
         store:store,
-        resave:true,    // false = 12:00am auth 1pm => 3:00pm Active  
+        resave:true,
         saveUninitialized:true,
     })
 );
 
-app.use(function (req,res,next) {                 // Memberimizni qiymatini olish uchun  
+app.use(function (req,res,next) {
     const sessionInstance = req.session as T;
     res.locals.member = sessionInstance.member;
     next();
 });
-app.use(express.json());
-
-
 
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTES **/
-app.use('/admin', routerAdmin);      // SSR : EJS 
+app.use('/admin', routerAdmin);
+app.use('/', router);
 
-app.use('/', router);               // SPA: REACT  API service sfatida ishlatish
-
-app.use('/', router);                 // SPA: REACT  API service sfatida ishlatish
-
-
-
-
-
-export default app;     // C.J => module.exports =app
+export default app;
